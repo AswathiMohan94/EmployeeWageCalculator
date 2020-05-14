3acc29d61e6d30e91b3497901048f80b71e1db56
AswathiMohan94 <aswathimohantly@gmail.com>
2020-05-14 23:01:59 +0530

    [Aswathi]ADDED : added employee wage uc1

---
+++ src/main/java/com/bridgelabz/employee/EmployeeWageBuilder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employee;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args) {
+        int IsFullTime = 1;
+        double empAttendance = Math.floor(Math.random() * 10) % 2;
+        if (empAttendance == IsFullTime) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee Absent");
+        }
+    }
+}
