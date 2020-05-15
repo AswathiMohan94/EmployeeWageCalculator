6043f62e2d94bfb3e4c638c5526a6538d7fdff61
AswathiMohan94 <aswathimohantly@gmail.com>
2020-05-15 14:14:06 +0530

    [Aswathi]ADDED :added employee wage and total employee wage using forloop

--- src/main/java/com/bridgelabz/employee/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/employee/EmployeeWageBuilder.java
@@ -3,24 +3,32 @@ package com.bridgelabz.employee;
 public class EmployeeWageBuilder {
 
     public static final int IsFullTime = 2;
-    public static final int IsPartTime=1;
-
+    public static final int IsPartTime = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
+    private static final int NUM_OF_WORKING_DAYS = 5;
 
     public static void main(String[] args) {
         int employeeHrs = 0;
         int employeeWage = 0;
-         double empAttendance = Math.floor(Math.random() * 10) % 3;
-        switch((int)empAttendance) {
-            case IsFullTime:
-                employeeHrs = 8;
-                break;
-            case IsPartTime:
-                employeeHrs = 4;
-                break;
+        int totalWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            double empAttendance = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empAttendance) {
+                case IsFullTime:
+                    employeeHrs = 8;
+                    break;
+                case IsPartTime:
+                    employeeHrs = 4;
+                    break;
+                default:
+                    employeeHrs = 0;
+            }
+            employeeWage = employeeHrs * EMP_RATE_PER_HOUR;
+            totalWage+= employeeWage;
+            System.out.println("Employee wage " + employeeWage);
         }
-        employeeWage = employeeHrs * EMP_RATE_PER_HOUR;
-         System.out.println("Employee wage "+employeeWage);
-        }
+
+        System.out.println("Total Employee wage " + totalWage);
     }
+}
 
