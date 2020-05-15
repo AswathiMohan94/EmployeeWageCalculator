90ff9f1c82484b107fa148f51dfb78b20c3d8280
AswathiMohan94 <aswathimohantly@gmail.com>
2020-05-15 13:58:25 +0530

    [Aswathi]Added : user regstration using switch

--- src/main/java/com/bridgelabz/employee/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/employee/EmployeeWageBuilder.java
@@ -1,13 +1,26 @@
 package com.bridgelabz.employee;
 
 public class EmployeeWageBuilder {
+
+    public static final int IsFullTime = 2;
+    public static final int IsPartTime=1;
+
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        int IsFullTime = 1;
-        double empAttendance = Math.floor(Math.random() * 10)%2;
-        if (empAttendance == IsFullTime) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee Absent");
+        int employeeHrs = 0;
+        int employeeWage = 0;
+         double empAttendance = Math.floor(Math.random() * 10) % 3;
+        switch((int)empAttendance) {
+            case IsFullTime:
+                employeeHrs = 8;
+                break;
+            case IsPartTime:
+                employeeHrs = 4;
+                break;
+        }
+        employeeWage = employeeHrs * EMP_RATE_PER_HOUR;
+         System.out.println("Employee wage "+employeeWage);
         }
     }
-}
+
